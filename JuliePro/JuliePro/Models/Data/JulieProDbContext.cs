a950b643fef3f532fd3e9302de0268f2b01106f7
Joyce0501 <1983276@cegepmontpetit.ca>
2021-09-21 09:33:54 -0400

    REFACTOR DataAccess DbContext
    Creation du DataAccess

--- JuliePro/JuliePro/Models/Data/JulieProDbContext.cs
+++ JuliePro/JuliePro/Models/Data/JulieProDbContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace JuliePro.Models.Data
 {
-    public class JulieProDbContext
+    public class JulieProDbContext: DbContext
     {
+        public JulieProDbContext(DbContextOptions<JulieProDbContext> options) : base(options)
+        {
+
+        }
+
     }
 }
---
+++ JuliePro/JuliePro_DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JuliePro_DataAccess
+{
+    public class Class1
+    {
+    }
+}
