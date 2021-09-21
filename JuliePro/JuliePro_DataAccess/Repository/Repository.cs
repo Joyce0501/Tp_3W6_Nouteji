4404df8be33df908824ade3db48b11a701a6c35a
Joyce0501 <1983276@cegepmontpetit.ca>
2021-09-21 10:06:55 -0400

    UnitOfWork
    Mise en place du  UnitOfWork et du repository Patterns (générique)

---
+++ JuliePro/JuliePro_DataAccess/Repository/Repository.cs
@@ -0,0 +1,88 @@
+﻿using JuliePro.Models.Data;
+using JuliePro_DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuliePro_DataAccess.Repository
+{
+     public class Repository<T> : IRepository<T> where T : class
+    {
+        // pour accéder à la BD, aux entités
+        private readonly JulieProDbContext _db;
+
+        // pour faire des changements directs
+        internal DbSet<T> dbset;
+
+        public Repository(JulieProDbContext db)
+        {
+            _db = db;
+            this.dbset = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbset.Add(entity);
+        }
+
+        public T Find(int id)
+        {
+            return dbset.Find(id);
+        }
+
+        public T FirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbset;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    // reproduit: _db.Product.Include(u => u.Category).Include(u => u.Size)
+                    // mais passé en string
+                    query = query.Include(includeProp);
+                }
+            }
+
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            return query.FirstOrDefault();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null, bool isTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
