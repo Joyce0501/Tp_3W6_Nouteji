f90ac014a2ee923dbeae9921f994b7480ba026ab
Joyce0501 <1983276@cegepmontpetit.ca>
2021-09-29 11:54:56 -0400

    Fct CONFIG i18n
    Configuration de l’internationalisation

--- JuliePro/JuliePro/Startup.cs
+++ JuliePro/JuliePro/Startup.cs
@@ -5,12 +5,15 @@ using JuliePro_DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -28,6 +31,25 @@ namespace JuliePro
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Localization
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddControllersWithViews().AddRazorRuntimeCompilation()
+                    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                    .AddDataAnnotationsLocalization();
+
+            /*.AddDataAnnotationsLocalization(options => {
+                   options.DataAnnotationLocalizerProvider = (type, factory) =>
+                       factory.Create(typeof(SharedResource));
+                   }); */
+
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+
+            #endregion
             services.AddDbContext<JulieProDbContext>(options =>
       options.UseSqlServer(
         Configuration.GetConnectionString("DefaultConnection")));
@@ -63,5 +85,12 @@ namespace JuliePro
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+        private CultureInfo[] supportedCultures = new[]
+        {
+                new CultureInfo("en-US"),
+                new CultureInfo("fr-CA")
+            };
+
     }
 }
