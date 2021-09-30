88d3189316fa5f15c9bda84b4307d17dea726c22
Joyce0501 <1983276@cegepmontpetit.ca>
2021-09-30 14:59:38 -0400

    Fct migration liée a session
    Update liaisons

---
+++ JuliePro/JuliePro_DataAccess/Migrations/20210930185744_CustTrainSession.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JuliePro_DataAccess.Migrations
+{
+    public partial class CustTrainSession : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
