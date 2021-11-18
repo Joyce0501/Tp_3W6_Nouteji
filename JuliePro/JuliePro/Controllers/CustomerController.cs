faf4747d66690b6e55e5f5560e1f65447eb68f83
Joyce <1983276@cegepmontpetit.ca>
2021-11-18 15:06:59 -0500

    Progres creation de la customercard

---
+++ JuliePro/JuliePro/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using JuliePro.Utility;
+using JuliePro_DataAccess.Repository.IRepository;
+using JuliePro_Models.Models;
+using JuliePro_Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+namespace JuliePro.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<CustomerController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public CustomerController(IUnitOfWork unitOfWork, ILogger<CustomerController> logger, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public async Task<IActionResult> CustomerCard()
+        {
+            IEnumerable<Customer> CustomerList = await _unitOfWork.Customer.GetAllAsync();
+
+            return View(CustomerList);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            CustomerVM CustomerVM = new CustomerVM()
+            {
+                Customer = await _unitOfWork.Customer.FirstOrDefaultAsync(c => c.Id == id),
+                ObjectiveList = await _unitOfWork.Objective.GetAllAsync(o => o.Customer_Id == id, includeProperties: "Subject"),
+                ScheduledSessionList = await _unitOfWork.ScheduledSession.GetAllAsync(s => s.Customer_Id == id, includeProperties: "Subject")
+            };
+            return View(CustomerVM);
+        }
+    }
+
+ 
+}
---
+++ JuliePro/JuliePro_Models/ViewModels/CustomerVM.cs
@@ -0,0 +1,16 @@
+﻿using JuliePro_Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuliePro_Models.ViewModels
+{
+    public class CustomerVM
+    {
+        public Customer Customer { get; set; }
+        public IEnumerable<Objective> ObjectiveList { get; set; }
+        public IEnumerable<ScheduledSession> ScheduledSessionList { get; set; }
+    }
+}
